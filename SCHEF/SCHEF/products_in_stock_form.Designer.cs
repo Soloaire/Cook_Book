c9019844e485b19cd718e85963ccf380490b7e7c
Stanislav <stanislav.mina@gmail.com>
2022-06-05 23:18:04 +0300

    Добавил БД с ингридиентами

--- SCHEF/SCHEF/products_in_stock_form.Designer.cs
+++ SCHEF/SCHEF/products_in_stock_form.Designer.cs
@@ -29,26 +29,112 @@
         private void InitializeComponent()
         {
             this.back_button = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ingredient_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Count = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // back_button
             // 
-            this.back_button.Location = new System.Drawing.Point(1177, 12);
+            this.back_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.back_button.Location = new System.Drawing.Point(1780, 5);
+            this.back_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.back_button.Name = "back_button";
-            this.back_button.Size = new System.Drawing.Size(75, 23);
+            this.back_button.Size = new System.Drawing.Size(112, 35);
             this.back_button.TabIndex = 0;
             this.back_button.Text = "Назад";
             this.back_button.UseVisualStyleBackColor = true;
             this.back_button.Click += new System.EventHandler(this.back_button_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.ingredient_name,
+            this.Count,
+            this.Price});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.No;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(1510, 832);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.back_button, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1896, 1048);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // id
+            // 
+            this.id.Frozen = true;
+            this.id.HeaderText = "id";
+            this.id.MinimumWidth = 8;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            this.id.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.id.Width = 150;
+            // 
+            // ingredient_name
+            // 
+            this.ingredient_name.Frozen = true;
+            this.ingredient_name.HeaderText = "Name";
+            this.ingredient_name.MinimumWidth = 8;
+            this.ingredient_name.Name = "ingredient_name";
+            this.ingredient_name.ReadOnly = true;
+            this.ingredient_name.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.ingredient_name.Width = 300;
+            // 
+            // Count
+            // 
+            this.Count.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.Count.Frozen = true;
+            this.Count.HeaderText = "Количество";
+            this.Count.MinimumWidth = 8;
+            this.Count.Name = "Count";
+            this.Count.ReadOnly = true;
+            this.Count.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Count.Width = 200;
+            // 
+            // Price
+            // 
+            this.Price.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Price.HeaderText = "цена";
+            this.Price.MinimumWidth = 8;
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
             // products_in_stock_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
-            this.Controls.Add(this.back_button);
+            this.ClientSize = new System.Drawing.Size(1896, 1048);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "products_in_stock_form";
             this.Text = "Продукты на складе";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +142,11 @@
         #endregion
 
         private System.Windows.Forms.Button back_button;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ingredient_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Count;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
     }
 }
--- SCHEF/SCHEF/products_in_stock_form.cs
+++ SCHEF/SCHEF/products_in_stock_form.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SCHEF
 {
     public partial class products_in_stock_form : Form
     {
+
+        DataBase database = new DataBase();
+
         public products_in_stock_form()
         {
             InitializeComponent();
+
+            LoadData();
+        }
+
+
+        private void LoadData()
+        {
+            
+
+            database.openConnection();
+
+            string query = "SELECT * FROM ingredients ORDER BY id";
+
+            SqlCommand command = new SqlCommand(query, database.getConnection());
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<string[]> data = new List<string[]>();
+
+            while (reader.Read())
+            {
+                data.Add(new string[4]);
+
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[1].ToString();
+                data[data.Count - 1][2] = reader[2].ToString();
+                data[data.Count - 1][3] = reader[3].ToString();
+            }
+
+            reader.Close();
+
+            database.closeConnection();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
         }
 
         private void back_button_Click(object sender, EventArgs e) // кнопка "назад"
@@ -23,5 +62,6 @@ namespace SCHEF
             main_page mp = new main_page();
             mp.Show();
         }
+
     }
 }
